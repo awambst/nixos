afb655214182833b8af490e913210448a257859b
Arthur Wambst <arthur.wambst@epita.fr>
2025-11-18 13:07:44 +0100

    fix: touchpad

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,14 @@ in
   programs.hyprland.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
+  services.libinput = {
+    enable = true;
+    touchpad = {
+      tapping = true; # tap-to-click
+      naturalScrolling = true; # optional, scrolling direction
+      disableWhileTyping = true; # optional but recommended
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."${info.login}" = {
